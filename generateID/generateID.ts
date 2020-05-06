c3d9e3a8b8ab596c62e4beb01d9dd3f54e1b326f
Bruno Ponsoni Costa <42554984+psonybruno@users.noreply.github.com>
2020-05-06 11:01:47 -0300

    Update generateID.ts

--- generateID/generateID.ts
+++ generateID/generateID.ts
@@ -1,6 +1,6 @@
 import crypto = require('crypto');
 
-export const generateUniqueId = (size: number, type?: string): string =>
+export const generateID = (size: number, type?: string): string =>
 {
     return crypto.randomBytes(size).toString(type || 'HEX');
 }
