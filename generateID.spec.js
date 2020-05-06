ba71a736b305b051b9261b8c03aa921e9ea2447e
Bruno Ponsoni Costa <42554984+psonybruno@users.noreply.github.com>
2020-05-06 10:54:37 -0300

    Delete generateID.spec.js

--- generateID.spec.js
+++
@@ -1,8 +0,0 @@
-const generateUniqueId = require('./generateUnitId');
-
-describe('Generate a ID', () => {
-    it('should return an ID', () => {
-        const id = generateUniqueId();
-        expect(id).toHaveLength(10);
-    })
-})
